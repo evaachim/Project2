a4eae0bde851a6ccea4fd77debb653be3e364b12
Eva Achim <eva.achim@bison.howard.edu>
2018-11-15 19:36:30 -0600

    Comments

--- project2.s
+++ project2.s
@@ -8,18 +8,21 @@
 .text
 
 ErrorLong:
+#prints message for input that is too long
   la $a0, long
   li $v0, 4
   syscall
   j end
 
 ErrorInvalid:
+#prints message for invalid input
   la $a0, invalid
   li $v0, 4
   syscall
   j end
 
 ErrorEmpty:
+#prints message for empty string
   la $a0, empty
   li $v0, 4
   syscall
@@ -33,6 +36,7 @@ main:
   syscall
 
 Rid:
+#takes care of leading spaces
 li $t9, 32 # space
 lb $t8, 0($a0)
 beq $t9, $t8, Character
@@ -58,16 +62,20 @@ addi $a0, $a0, 1
 addi $t0, $t0, 1
 j traverse
 
+#busted empty space or input that violates limit
 discovered:
+#if it's empty go to case for empty which outputs 
 beqz $t0, ErrorEmpty
 slti $t4, $t0, 5
+#if it's too long, go to case for too long and print 
 beqz $t4, ErrorLong
 move $a0, $t2
+#go to next verification process
 j verify
 
 #checks inputs
 verify:
-lb $s3, 0($a0)
+lb $s3, 0($a0)  #loads address here
 beqz $s3, initial
 beq $s3, $t1, initial
 slti $t3, $s3, 48                 #invalid for anything below 0
@@ -84,15 +92,18 @@ slti $t3, $s3, 120                #legal input for lower case characters
 bne $t3, $zero, Move
 bgt $s3, 119, ErrorInvalid   # illegal input, out of range
 
+#now I iterate again, this time to check for invalid input
 Move:
 addi $a0, $a0, 1 #iterates
 j verify #goes to verification point
 
+#first step of conversion, does the prerequisite work for translation 
 initial:
 move $a0, $t2  #moves content
 addi $t5, $t5, 0  #$t5 has 0 now
 add $s0, $s0, $t0  
 addi $s0, $s0, -1 #decrement	
+#load immediate puts values in registers to be used
 li $s4, 3  #each digit
 li $s5, 2
 li $s6, 1
@@ -118,14 +129,15 @@ addi $s7, $s7, -55  #conversion for upper case
 j row
 
 Mici:
-addi $s7, $s7, -87  #conversion for lowe case
+addi $s7, $s7, -87  #conversion for lower case
 
-row:
-beq $s0, $s4, one  #sequential checks 
+row:  #determines which digit needs to be converted
+beq $s0, $s4, one  
 beq $s0, $s5, two
 beq $s0, $s6, three
 beq $s0, $s1, last
 
+#first character
 one:
 li $t6, 35937   #values to multiply by for the power of 3
 mult $s7, $t6
@@ -135,6 +147,7 @@ addi $s0, $s0, -1
 addi $a0, $a0, 1
 j translate
 
+#second character
 two:
 li $t6, 1089   #values to multiply by for the power of 2
 mult $s7, $t6
@@ -144,7 +157,7 @@ addi $s0, $s0, -1
 addi $a0, $a0, 1
 j translate
 
-
+#third character
 three:
 li $t6, 33   #values to multiply by for the power of 1
 mult $s7, $t6
@@ -154,19 +167,21 @@ addi $s0, $s0, -1
 addi $a0, $a0, 1
 j translate
 
+#fourth character
 last:
 li $t6, 1    #values to multiply by for the power of 0
 mult $s7, $t6
 mflo $t7
 add $t5, $t5, $t7
+#no more need to go back to translation step
 
 
-
-final:                  #final step, moves content to $a0
+final:                  #final step, moves content to $a0 so it can be printed
 li $v0, 1
 move $a0, $t5
 syscall
 
+#last system call of the program will end program
 end:   #prints result
  li $v0, 10
  syscall

1944983baf544557cbf3a1202dd9693f337afd8f
Eva Achim <eva.achim@bison.howard.edu>
2018-11-16 14:02:16 -0600

    I added a nice heading

--- project2.s
+++ project2.s
@@ -1,12 +1,11 @@
+############### Base 33 to Base 10 Converter ###################
 .data
   userInput:    .space  700
   empty:   .asciiz "Input is empty."
    long:    .asciiz "Input is too long."
   invalid: .asciiz "Invalid base-33 number."
 
-
 .text
-
 ErrorLong:
 #prints message for input that is too long
   la $a0, long
@@ -27,7 +26,6 @@ ErrorEmpty:
   li $v0, 4
   syscall
   j end
-
 
 main:
   li $v0, 8

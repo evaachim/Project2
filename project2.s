0c44e9f9e308113f8b7e4321c4adc8a15d1e3585
Eva Achim <eva.achim@bison.howard.edu>
2018-11-15 18:24:44 -0600

    Checks for range if input out of range, ErrorInvalid goes to print error message

--- project2.s
+++ project2.s
@@ -81,6 +81,8 @@ slti $t3, $s3, 97                 # invalid input, not numerical nor alphabetica
 bne $t3, $zero, ErrorInvalid
 slti $t3, $s3, 120                #legal input for lower case characters
 bne $t3, $zero, Move
+bgt $s3, 119, ErrorInvalid   # illegal input, out of range
+
 Move:  #now we iterate again, this time to check for invalid input
 addi $a0, $a0, 1 #iterates
 j verify #goes to verification point
